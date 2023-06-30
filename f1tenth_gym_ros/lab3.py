f53cfaf721152dc4dc3e46d45ec94ccc8f9f1160
Jan Strehl <jan.david.strehl@googlemail.com>
2023-06-30 19:34:49 +0300

    fix: added dt

--- f1tenth_gym_ros/lab3.py
+++ f1tenth_gym_ros/lab3.py
@@ -258,7 +258,7 @@ class Lab3(Node):
         R_t_1 = np.eye(3)*0.05 #initial guess --> to Tune 
         print("R_t_1")
         print(R_t_1)
-        G_t_1 = np.array([[1, 0, -v_t_1*np.sin(mu_t[2])], [0, 1, v_t_1*np.cos(mu_t[2])], [0, 0, 1]])
+        G_t_1 = np.array([[1, 0, -v_t_1*np.sin(mu_t[2])*self.dt], [0, 1, v_t_1*np.cos(mu_t[2])*self.dt], [0, 0, 1]])
         Q_t_1 = measured_covariance #covariance matrix of measurement noise: assumption, usually it is tuned 
          
         # prediction step 
